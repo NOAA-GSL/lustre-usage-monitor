d53f18b200ad490d361f42bb8ab246114ca00744
Samuel Trahan <Samuel.Trahan@noaa.gov>
2021-03-10 13:50:54 +0000

    Do not directly copy report to desitination jet.txt on jet

--- scripts/final_report.sh
+++ scripts/final_report.sh
@@ -59,7 +59,7 @@ github_deliver() {
 }
 
 if [[ -d /lfs4 ]] ; then
-    /bin/cp -fpL "${USAGE_MONITOR:-$HOME/lustre-usage-monitor}"/out/report.txt /lfs1/BMC/rtfim/disk-usage/jet.txt
+    #/bin/cp -fpL "${USAGE_MONITOR:-$HOME/lustre-usage-monitor}"/out/report.txt /lfs1/BMC/rtfim/disk-usage/jet.txt
     system=jet
 elif [[ -d /scratch1/NCEPDEV ]] ; then
     #scp "${USAGE_MONITOR:-$HOME/lustre-usage-monitor}"/out/report.txt jetscp.rdhpcs.noaa.gov:/lfs1/BMC/rtfim/disk-usage/hera.txt
