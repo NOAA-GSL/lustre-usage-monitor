463fc191e89a4df3f48394e5b2dc77c810daf90c
Samuel Trahan <Samuel.Trahan@noaa.gov>
2024-05-24 13:08:43 -0500

    hercules usage monitor xml and sh

---
+++ scripts/hercules-3day-rocoto.sh
@@ -0,0 +1,35 @@
+#! /bin/bash --login
+
+# source /apps/lmod/lmod/init/bash
+
+# module load contrib/0.1
+# module load rocoto/1.3.3
+
+export PATH=/apps/contrib/rocoto/1.3.6/bin/:$PATH
+
+set -e 
+
+cd "$USAGE_MONITOR"
+
+echo -n "$USAGE_MONITOR" > topdir.ent
+
+areas=( $( cat hercules-disk-areas.lst ) )
+echo " " ${areas[@]} | sed 's, /, ,g ; s,/,--,g' > dir-entity-list.ent
+
+origin=$( date +%s -d "2024-05-24t00:00:00" )
+now=$( date +%s )
+delta=$(( (now-origin) % (3600*24*3) - 1 ))
+deltam2=$(( delta + 3600*24*3*3 ))
+deltap1=$(( delta - 3600*24*3*2 ))
+
+ymd00m2=$( date +%Y%m%d -d "$deltam2 seconds ago" )0000
+ymd00p1=$( date +%Y%m%d -d "$deltap1 seconds ago" )0000
+
+echo "$ymd00m2 $ymd00p1 72:00:00" > cycledef.ent
+
+which rocotorun > /dev/null
+if [[ -t 1 ]] ; then
+    rocotorun -w hercules-disk-usage.xml -d hercules-disk-usage.db --verbose 10
+else
+    rocotorun -w hercules-disk-usage.xml -d hercules-disk-usage.db
+fi
