d91eca28d3a70f1005a808664d160b96b7dad9f9
Samuel Trahan <Samuel.Trahan@noaa.gov>
2026-02-03 11:54:18 -0500

    add gpl v3 copyright information

--- scripts/rm-usage-tmp.bash
+++ scripts/rm-usage-tmp.bash
@@ -1,3 +1,9 @@
 #! /bin/bash
 
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+
 find "${USAGE_MONITOR:-$HOME/lustre-usage-monitor}" -name '*-*.tmp' -a -mmin +30 -print0 | xargs -0 rm -f
