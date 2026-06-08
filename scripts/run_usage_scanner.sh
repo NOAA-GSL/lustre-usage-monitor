6f82e326fc158f2660609dc9701afdc241f2e084
samuel.trahan <Samuel.Trahan@noaa.gov>
2026-06-08 17:57:53 +0000

    another bug fix to generate full & short per-directory files

--- scripts/run_usage_scanner.sh
+++ scripts/run_usage_scanner.sh
@@ -11,10 +11,12 @@ if [[ ! -d "${dir}" ]] ; then
     mkdir "${dir}"
 fi
 
+set +e
+
 $disk_usage_program \
     -t "$restart_interval" -d "${dir}.done" \
     -r `echo "/${dir}" | sed s,--,/,g` "${dir}.rst.gz" "${dir}/" \
     -o "${dir}.xml"
 
 $report_maker short "${dir}.xml" > "${dir}.txt"
-$report_maker long "${dir}-full.xml" > "${dir}-full.txt"
+$report_maker long "${dir}.xml" > "${dir}-full.txt"
